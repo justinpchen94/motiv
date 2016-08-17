c93863bd0994321e19fb98c8b21768286d2b3d21
Justin Chen <justinpchen94@gmail.com>
2016-08-17 15:13:58 -0700

    [bug] fix profile state

--- app/components/Account/profile/Profile.js
+++ app/components/Account/profile/Profile.js
@@ -25,7 +25,8 @@ class ReadOnlyProfile extends React.Component {
       goals: this.props.goals || [],
       stats: this.props.stats || [],
       milestones: this.props.milestones || [],
-      contents: this.props.contents || []
+      contents: this.props.contents || [],
+      diets: this.props.diets || []
     };
   }
 
@@ -35,13 +36,18 @@ class ReadOnlyProfile extends React.Component {
       goals: nextProps.goals,
       stats: nextProps.stats,
       milestones: nextProps.milestones,
-      contents: nextProps.contents
+      contents: nextProps.contents,
+      diets: nextProps.diets
     });
   }
 
 
 
   render() {
+    const foodDOM = _.map(this.state.diets, (food,index) => {
+      return (<RegimeDiet key={index} {...food}/>);
+    });
+
     const statList = _.map(this.state.stats, (stat, index) => {
       return (<Stat key={index} {...stat} />);
     });
@@ -54,57 +60,50 @@ class ReadOnlyProfile extends React.Component {
       return (<Milestone key={index} {...milestone} />);
     });
 
+    const followerButton = (this.state.user.id === this.props.loggedInUser.id) ? '' : (<AddFriendButton {...this.state} />) 
+
     return (
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col-md-2">
+    <div>
+      <ul className="nav nav-tabs">
+        <li className="active"><a href="#profile" data-toggle="tab">Profile</a></li>
+        <li><a href="#diet" data-toggle="tab">Diet</a></li>
+        <li><a href="#supplement" data-toggle="tab">Supplement</a></li>
+        <li><a href="#exercise" data-toggle="tab">Exercise</a></li>
+        <li><a href="#timeline" data-toggle="tab">Timeline</a></li>
+      </ul>
+      <div id="myTabContent" className="tab-content">
+        <div className="tab-pane fade active in" id="profile">
           <Messages messages={this.props.messages}/>
-          <Bio {...this.state.user} />
-
-          <hr />
-
-          <div className="bio-div">
-            <h4>Stat</h4>
-              {statList}
-          </div>
-
-          <hr />
-
-          <div className="bio-div">
-            <h4>Goals</h4>
-              {goalList}
-          </div>
-
-          <hr />
-
-          <div className="bio-div">
-            <h4>Milestone</h4>
-              {milestoneList}
-          </div>    
-        </div>
-        <div className="col-md-10">
-          <div className="pull-right"><AddFriendButton {...this.state.user} /></div>
-          <ul className="nav nav-tabs">
-            <li className="active"><a href="#diet" data-toggle="tab">Diet</a></li>
-            <li><a href="#supplement" data-toggle="tab">Supplement</a></li>
-            <li><a href="#exercise" data-toggle="tab">Exercise</a></li>
-            <li><a href="#timeline" data-toggle="tab">Timeline</a></li>
-          </ul>
-          <div id="myTabContent" className="tab-content">
-            <div className="tab-pane fade active in" id="diet">
-              <Diet />
+            {followerButton}
+            <h4>Profile Information</h4>
+              <Bio {...this.state.user} />
+                <div>
+                  <h4>Stat</h4>
+                    {statList}
+                </div>
+                <div>
+                  <h4>Goals</h4>
+                    {goalList}
+                </div>
+                <div>
+                <h4>Milestone</h4>
+                  {milestoneList}
+                </div>
             </div>
-            <div className="tab-pane fade" id="supplement">
-              <Supplements />
-            </div>
-            <div className="tab-pane fade" id="exercise">
-              <Exercise />
-            </div>
-            <div className="tab-pane fade" id="timeline">
-              <Timeline />
-            </div>
-          </div>
-        </div>
+      <div className="tab-pane fade" id="diet">
+        <Diet />
+        {foodDOM}
+      </div>
+      <div className="tab-pane fade" id="supplement">
+        <Supplements />
+      </div>
+      <div className="tab-pane fade" id="exercise">
+        <Exercise />
+      </div>
+      <div className="tab-pane fade" id="timeline">
+        <Timeline />
+      </div>
+
       </div>
     </div>
     );
@@ -118,7 +117,9 @@ const mapStateToProps = (state) => {
     goals: state.profile.goals,
     stats: state.profile.stats,
     milestones: state.profile.milestones,
-    messages: state.messages
+    messages: state.messages,
+    diets:state.regime.diet,
+    loggedInUser: state.auth.user
   };
 };
 
